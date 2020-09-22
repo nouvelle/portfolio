17dba1ace839ed790c37836a7c01386d82b2a3a4
nouvelle <nouvellenuts@gmail.com>
2020-09-22 23:36:45 +0900

    Add work page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
-import Work from '../components/Work';
 
 import pic1 from '../assets/images/pic01.jpg';
 import pic2 from '../assets/images/pic02.jpg';
 import pic3 from '../assets/images/pic03.jpg';
+import port1 from '../assets/images/port01.png';
+import port2 from '../assets/images/port02.png';
 
 import config from '../../config';
 const IndexPage = () => (
@@ -42,24 +43,24 @@ const IndexPage = () => (
 
       <section id="two" className="wrapper alt spotlight style2">
         <div className="inner">
-          <a href="#work" className="image">
+          <Link to="/work" className="image">
             <img src={pic2} alt="skills" />
-          </a>
+          </Link>
           <div className="content">
             <h2 className="major">Skills</h2>
             <p>
               I’m a software engineer with passion for the front-end.
             </p>
-            <a href="#work" className="special">
+            <Link to="/work" className="special">
               My portfolio
-            </a>
+            </Link>
           </div>
         </div>
       </section>
 
       <section id="three" className="wrapper spotlight style3">
         <div className="inner">
-          <a href="/#" className="image">
+          <a href="https://wonderful-dubinsky-d7fc26.netlify.app/" className="image">
             <img src={pic3} alt="interests" />
           </a>
           <div className="content">
@@ -76,7 +77,36 @@ const IndexPage = () => (
         </div>
       </section>
 
-      <Work />
+      <section id="work" className="wrapper alt style1">
+      <div className="inner">
+        <h2 className="major">Work</h2>
+        <section className="features">
+          <article>
+            <Link to="/work/MY-STARBUCKS-JOURNEY/" className="image">
+              <img src={port1} alt="MY-STARBUCKS-JOURNEY" />
+            </Link>
+            <h3 className="major">MY-STARBUCKS-JOURNEY</h3>
+            <p>This is my Starbucks journey in Seattle using Deck.gl made by UBER.</p>
+            <Link to="/work/MY-STARBUCKS-JOURNEY/" className="special">
+              More
+            </Link>
+          </article>              
+          <article>
+            <Link to="/work/QLAB/" className="image">
+              <img src={port2} alt="QLAB" />
+            </Link>
+            <h3 className="major">QLAB</h3>
+            <p>QLAB is a learning envioronment where you can learn any topic to earn points.</p>
+            <Link to="/work/QLAB/" className="special">
+              More
+            </Link>
+          </article>              
+        </section>
+        <Link to="/work" className="special right">
+          Check more
+        </Link>
+      </div>
+    </section>
     </section>
   </Layout>
 );
